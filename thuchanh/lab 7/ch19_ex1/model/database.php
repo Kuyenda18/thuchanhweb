ac40399372d3d22a8dab5eb6a451b4614c34811c
Kuyenda18 <phat123xa@gmail.com>
2023-11-01 08:16:03 +0700

    lab 7
    Exercise 19

---
+++ thuchanh/lab 7/ch19_ex1/model/database.php
@@ -0,0 +1,20 @@
+<?php
+    $dsn = 'mysql:host=localhost;port=3307;dbname=my_guitar_shop1';
+    $username = 'root';
+    $password = 'Kuyenda18.01.2004';
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+try {
+    $db = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include 'errors/db_error_connect.php';
+    exit;
+}
+
+function display_db_error($error_message) {
+    global $app_path;
+    include 'errors/db_error.php';
+    exit;
+}
+?>
